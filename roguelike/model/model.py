88ea0030df42ffbe02d60c85ecc0ad8c46bb3e3a
kwoolter <keith.woolterton@gmail.com>
2020-06-14 13:59:46 +0800

    Added FloorView

--- roguelike/model/model.py
+++ roguelike/model/model.py
@@ -1,4 +1,5 @@
 import numpy as np
+import random
 
 class Entity():
     def __init__(self, name:str, x:int, y:int):
@@ -10,6 +11,16 @@ class Entity():
     def xy(self):
         return(self.x, self.y)
 
+    @xy.setter
+    def xy(self, new_xy):
+        x,y = new_xy
+        self.x = x
+        self.y = y
+
+    def move(self, dx:int, dy:int):
+        self.x += dx
+        self.y += dy
+
 class Player(Entity):
     def __init__(self, name: str, x:int = 1, y:int = 1):
         super().__init__(name,x,y)
@@ -20,19 +31,37 @@ class Floor():
         self.width = width
         self.height = height
         self.player = None
+        self.entities = []
 
-        self.map = np.array(dtype = int)
+    def initialise(self):
+        for i in range(10):
+            self.entities.append(Entity(name="NPC",
+                                        x=random.randint(1,self.width),
+                                        y=random.randint(1,self.height)))
+
+        # self.map = np.array(dtype = int)
 
     def add_player(self, new_player : Player, x:int = None, y:int = None):
+        if x is None:
+            x = int(self.width/2)
+        if y is None:
+            y = int(self.height/2)
+
         self.player = new_player
+        self.player.xy = (x,y)
+
 
 class Model():
     def __init__(self, name:str):
         self.name = name
         self.player = None
+        self.current_floor = None
 
     def initialise(self):
         self.add_player(Player(name="Keith"))
+        self.current_floor = Floor("Test", 50, 50)
+        self.current_floor.initialise()
+        self.current_floor.add_player(self.player)
 
     def tick(self):
         pass
@@ -44,5 +73,4 @@ class Model():
         return self.player
 
     def move_player(self, dx:int, dy:int):
-        self.player.x += dx
-        self.player.y += dy
+        self.player.move(dx, dy)
--- roguelike/view/view.py
+++ roguelike/view/view.py
@@ -17,6 +17,10 @@ class MainFrame():
         super().__init__()
         self.width = width
         self.height = height
+        self.game = None
+        self.con = None
+
+        self.floor_view = FloorView(self.width, self.height)
 
     def initialise(self, model: model.Model):
 
@@ -32,13 +36,37 @@ class MainFrame():
 
         self.con = libtcod.console_new(self.width, self.height)
 
+        self.floor_view.initialise(self.game.current_floor)
+
     def draw(self):
         player_x, player_y = self.game.player.xy
 
         libtcod.console_set_default_foreground(self.con, libtcod.white)
         libtcod.console_put_char(self.con, player_x, player_y, '@', libtcod.BKGND_NONE)
-        libtcod.console_blit(self.con, 0, 0, self.width, self.height, 0, 0, 0)
+
+        self.floor_view.draw()
+        libtcod.console_blit(self.floor_view.con, 0, 0, self.width, self.height, 0, 0, 0, ffade=0.7)
+        libtcod.console_blit(self.con, 0, 0, self.width, self.height, 0, 0, 0, ffade=0.7, bfade=0.5)
+
         libtcod.console_flush()
         libtcod.console_put_char(self.con, player_x, player_y, ' ', libtcod.BKGND_NONE)
 
+class FloorView(View):
+    def __init__(self, width:int, height:int):
+        self.width = width
+        self.height = height
+        self.floor = None
+        
+    def initialise(self, floor:model.Floor):
+        self.floor = floor
+        self.con = libtcod.console_new(self.width, self.height)
+        
+    def draw(self):
 
+        for e in self.floor.entities:
+            x,y = e.xy
+            libtcod.console_set_default_foreground(self.con, libtcod.yellow)
+            libtcod.console_put_char(self.con, x, y, '@', libtcod.BKGND_NONE)
+
+
+        
